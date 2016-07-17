bbdd8f503908bac381e6dea63e85a320020eb979
Yifei Yin <yifeiyin@foxmail.com>
2016-07-17 10:33:16 +0800

    Initial upload

---
+++ colourful.h
@@ -0,0 +1,37 @@
+#ifndef COLORFUL_H
+#define COLORFUL_H
+
+namespace COLORFUL
+{
+const char hBLACK[] = "\e[1;30mm";
+const char hRED[] = "\e[1;31mm";
+const char hGREEN[] = "\e[1;32mm";
+const char hORANGE[] = "\e[1;33m";
+const char hBLUE[] = "\e[1;34m";
+const char hPURPLE[] = "\e[1;35m";
+const char hBLUE_l[] = "\e[1;36m";
+const char hGRAY[] = "\e[1;37m";
+
+const char fBLACK[] = "\e[30m";
+const char fRED[] = "\e[31m";
+const char fGREEN[] = "\e[32m";
+const char fORANGE[] = "\e[33m";
+const char fBLUE[] = "\e[34m";
+const char fPURPLE[] = "\e[35m";
+const char fBLUE_l[] = "\e[36m";
+const char fGRAY[] = "\e[37m";
+
+const char bBLACK[] = "\e[40m";
+const char bRED[] = "\e[41m";
+const char bGREEN[] = "\e[42m";
+const char bORANGE[] = "\e[43m";
+const char bBLUE[] = "\e[44m";
+const char bPURPLE[] = "\e[45m";
+const char bBLUE_l[] = "\e[46m";
+const char bGRAY[] = "\e[47m";
+
+const char RESET[] = "\e[0m";
+
+}
+
+#endif // COLORFUL_H
