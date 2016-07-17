f74c1259585b132baf669945efe482a850c190e0
Yifei Yin <yifeiyin@foxmail.com>
2016-07-17 10:33:43 +0800

    Initial upload

---
+++ kbhit.h
@@ -0,0 +1,35 @@
+#ifndef KBHIT_H
+#define KBHIT_H
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
+
+int kbhit()
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+
+  return 0;
+}
+
+#endif // KBHIT_H
